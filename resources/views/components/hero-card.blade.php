f605234fdc7f7531faf5027310e8587f0b3793c4
marekgacek45 <marekgacek45@gmail.com>
2024-12-18 13:34:55 +0100

    fix bug

--- resources/views/components/hero-card.blade.php
+++ resources/views/components/hero-card.blade.php
@@ -6,7 +6,7 @@
      
 
         <img src="{{$img}}" alt="{{$alt}}"
-            class=" aspect-square sm:max-h-[450px] lg:max-h-[250px] 2xl:max-h-none {{$class}} rounded-lg ">
+            class=" aspect-square sm:max-h-[450px] lg:max-h-[250px] 2xl:max-h-none  rounded-lg object-cover  group-hover:scale-105 duration-300">
 
     </div>
     <div class="flex flex-col gap-3">
